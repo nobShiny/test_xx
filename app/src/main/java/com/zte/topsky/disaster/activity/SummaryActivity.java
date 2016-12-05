7efd0b5fba20e3aa5605e38defa71efc9259e4e9
nobShiny <496749946@qq.com>
2016-12-05 12:19:14 +0800

    2016.12.5 完成概述模块

--- app/src/main/java/com/zte/topsky/disaster/activity/SummaryActivity.java
+++ app/src/main/java/com/zte/topsky/disaster/activity/SummaryActivity.java
@@ -16,12 +16,29 @@ public class SummaryActivity extends BaseActivity {
 
     @BindView(R.id.tv_title_text)
     TextView tvTitleText;
+    @BindView(R.id.tv_summary)
+    TextView tvSummary;
+    @BindView(R.id.tv_river)
+    TextView tvRiver;
+    @BindView(R.id.tv_reservoir)
+    TextView tvReservoir;
+    @BindView(R.id.tv_complex)
+    TextView tvComplex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState, R.layout.activity_disaster_summary);
         addActivity(this);
         tvTitleText.setText("汛情摘要");
+        initData();
+    }
+
+    private void initData() {
+        tvSummary.setText("12月1日零晨1点到12月5日零晨1点，区域累计平均降雨量3.8毫米，其实1#检测点累计为0.6毫米，2#检测点累计为1.3毫米，3#检测点累计为1.9毫米，" +
+                "最大降雨量监测点为3#测站，较往年，冬季雨量偏少。");
+        tvRiver.setText("检测区域水量为安全水量。");
+        tvReservoir.setText("目前区域3#水库总蓄水量最大，为360立方米，平均蓄水量为31.2%，各水库水位均在安全线以下。");
+        tvComplex.setText("目前全区域汛情平稳。");
     }
 
 }
